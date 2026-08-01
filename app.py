90b92eba280813ebd03d13ad735face6e7ca9acd
antbek-cyber <antbek264@gmail.com>
2026-08-01 22:17:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def seed_data():
         workers = [
             ('Kwame Mensah', 'Plumber', 'Kumasi', 80.0, 5, 4.5, 12, '0241234567'),
             ('Ama Boateng', 'Electrician', 'Accra', 100.0, 3, 4.8, 20,'0559876543'),
-            ('Kofi Annan', 'Plumber', 'Kumasi', 70.0, 2, 4.0, 8, '0205554433,)
+            ('Kofi Annan', 'Plumber', 'Kumasi', 70.0, 2, 4.0, 8, '0205554433',)
         ]
         c.executemany("INSERT INTO workers (name, profession, location, price, experience, rating, total_ratings, phone) VALUES (?,?,?,?,?,?,?,?)", workers)
         conn.commit()
