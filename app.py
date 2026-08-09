fe864c6bce2de86faf0108702f4e6fba2292fd2c
antbek-cyber <antbek264@gmail.com>
2026-08-09 18:00:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -276,8 +276,8 @@ def post_job():
         budget = request.form['budget']
         
         db = get_db_connection()
-        db.execute("INSERT INTO jobs (customer_name, phone, job_type, location, description, budget, status) VALUES (?,?,?,?,?,?, 'open')",
-                   (name, phone, job_type, location, description, budget))
+        db.execute("INSERT INTO jobs (customer_name, phone, job_type, location, description, budget, status) VALUES (?,?,?,?,?,?)"
+                   (name, phone, job_type, location, description, budget, 'open'))
         db.commit()
         db.close()
         return redirect('/jobs')
