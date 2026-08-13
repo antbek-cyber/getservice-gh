75c18f6299ddf5141396b5481993a64cf8b3ec31
antbek-cyber <antbek264@gmail.com>
2026-08-13 12:59:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)  
 
 with app.app_context():
-    db.creat_all()
+    db.create_all()
 
 class Worker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
