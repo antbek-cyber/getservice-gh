710aba434fc2132351ecf07228ef49c83513d199
antbek-cyber <antbek264@gmail.com>
2026-08-13 22:52:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if database_url and database_url.startswith("postgres://"):
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlclhemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
