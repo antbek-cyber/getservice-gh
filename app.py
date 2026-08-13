9080063a18212b5db6a88e3312696ff23b4dd3ce
antbek-cyber <antbek264@gmail.com>
2026-08-13 12:16:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,25 @@ from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'super-secret-key-change-this'
+app.config['SECRET_KEY'] = 'super-secret-key-change-this'
 
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+# DATABASE SETUP FOR RENDER POSTGRES
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+db = SQLALclhemy(app)
+
+with app.app_context():
+    db.creat_all()
 
 class Worker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
