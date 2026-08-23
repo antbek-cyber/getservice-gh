c987786aa65238674ead63b5247ea79cd6791a0f
antbek-cyber <antbek264@gmail.com>
2026-08-23 07:07:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    print("!!! DB RECREATED WITH NEW SIZE !!!")
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
