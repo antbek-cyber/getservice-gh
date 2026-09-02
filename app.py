d72e6404f679fe7b027c83e024f7367e75e6612d
antbek-cyber <antbek264@gmail.com>
2026-09-02 03:28:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import os
 from PIL import Image
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
