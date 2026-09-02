87a2422b9c47c478abf7b16525bdd3ca79a6bff9
antbek-cyber <antbek264@gmail.com>
2026-09-02 22:31:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ class Worker(UserMixin, db.Model):
     rating = db.Column(db.Float, default=0)
     total_ratings = db.Column(db.Integer, default=0)
     photo = db.Column(db.String(500), default='default.png')
-    work_images = db.Column(db.text, default='')
+    work_images = db.Column(db.Text, default='')
     phone = db.Column(db.String(20), unique=True)
     password_hash = db.Column(db.String(500))
     is_approved = db.Column(db.Boolean, default=False)
