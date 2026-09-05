558712341e3e34c026a98b31a9868dcd3d5cde2d
antbek-cyber <antbek264@gmail.com>
2026-09-05 16:14:15 +0000

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,23 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-import os
-from PIL import Image
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
-from flask import request
-from sqlalchemy import or_
-from functools import wraps
-import cloudinary
-import cloudinary.uploader
-import math
-import io
-from datetime import datetime
-PAYSTACK_SECRET = os.environ.get('PAYSTACK_SECRET_KEY')
+from flask_login import LoginManager
 
-cloudinary.config(
-  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
-  api_key = os.getenv('CLOUDINARY_API_KEY'),
-  api_secret = os.getenv('CLOUDINARY_API_SECRET')
-  
-)
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'worker_login'
