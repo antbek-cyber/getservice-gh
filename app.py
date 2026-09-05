61acf9279ff245f0f37cd78554a2f319202dd34f
antbek-cyber <antbek264@gmail.com>
2026-09-05 16:09:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-import os
-from PIL import Image
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.utils import secure_filename
-from flask_sqlalchemy import SQLAlchemy
-from flask import request
-from sqlalchemy import or_
-from functools import wraps
-import cloudinary
-import cloudinary.uploader
-import math
-import io
-from datetime import datetime
-PAYSTACK_SECRET = os.environ.get('PAYSTACK_SECRET_KEY')
 
-cloudinary.config(
-  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
-  api_key = os.getenv('CLOUDINARY_API_KEY'),
-  api_secret = os.getenv('CLOUDINARY_API_SECRET')
-  
-)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key-change-this'
@@ -59,10 +37,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
  
-@login_manager.user_loader
-def load_user(user_id):
-     return Worker.query.get(int(user_id))
-
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -166,6 +140,10 @@ class Notification(db.Model):
 def index():
     services = Service.query.all()
     return render_template('index.html', services=services)
+  
+@login_manager.user_loader
+def load_user(user_id):
+     return Worker.query.get(int(user_id))
 
 @app.route('/add', methods=['POST'])
 def add_service():
