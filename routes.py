b20bd3ace44e7f779eb0ebeba4e3a9a5a83b1a89
antbek-cyber <antbek264@gmail.com>
2026-09-05 17:10:28 +0000

    Update routes.py

--- routes.py
+++ routes.py
@@ -20,9 +20,6 @@ UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
