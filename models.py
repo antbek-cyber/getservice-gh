2b7146bf8de24881dc288069ebe11043bb5863d5
antbek-cyber <antbek264@gmail.com>
2026-09-06 02:24:18 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from extensions import db
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.Model):
@@ -32,6 +33,14 @@ class Worker(UserMixin, db.Model):
     longitude = db.Column(db.Float, nullable=True)
     bio = db.Column(db.Text, nullable=True)
     fcm_token = db.Column(db.String(300))
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
