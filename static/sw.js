b5dc83d0a174e09d0a104f12fbcf2a78fad428d0
antbek-cyber <antbek264@gmail.com>
2026-09-20 05:25:50 +0000

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,13 @@
-self.addEventListener('push', function(e){
+self.addEventListener('push', function(e) {
   const data = e.data.json();
-  self.registration.showNotification("GetService-GH", {
+  self.registration.showNotification("GetService-GH 🔔", {
     body: data.message,
     icon: "/static/logo.png",
-    vibrate: [200,100,200]
+    vibrate: [300,100,300],
+    badge: "/static/logo.png"
   });
 });
-self.addEventListener('notificationclick', function(e){ e.notification.close(); e.waitUntil(clients.openWindow('/')); });
+self.addEventListener('notificationclick', function(e){
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
